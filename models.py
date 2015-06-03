181b1092fc956b4abed99947e1c4fd331d640859
Peter Koverda <koverda@gmail.com>
2015-06-03 15:40:09 -0700

    Getting my FK links working
    debugging my db, and getting all of my foreign key connections working.

--- models.py
+++ models.py
@@ -1,38 +1,36 @@
 from app import db
-from app import app
 
 class Survey(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(255), unique = True) # name of survey
-	list_q = db.Column(db.String(255)) # list of questions in query
-	responses = db.relationship ('Survey_Response', backref='parent', lazy='dynamic')
+	name = db.Column(db.String(64), index = True, unique = True)
+	list_q = db.Column(db.String(255), index = True) 
+	responses = db.relationship('Response', backref='parent', lazy='dynamic')
 	
 	def __repr__(self):
 		return '<Survey %r>' % (self.name)
 
-
-class Survey_Response(db.Model):
+class Response(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	id_surveys = db.Column(db.Integer, db.ForeignKey('Survey.id')) # associated survey
-	time_created = db.Column(db.DateTime) # when did we get this response
-	
-	def __repr__(self):
-		return '<Survey Response %r>' % (self.id)
-
-
-class Answer(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	id_questions = db.Column(db.Integer, db.ForeignKey('Question.id')) # associated question
-	id_survey_responses = db.Column(db.Integer, db.ForeignKey('Survey_Response.id')) # associated response
-	text_a = db.Column(db.String(511))
+	time_created = db.Column(db.DateTime)
+	id_surveys = db.Column(db.Integer, db.ForeignKey('survey.id'))
+	answers = db.relationship('Answer', backref='r_answer', lazy = 'dynamic')
 
 	def __repr__(self):
-		return '<Answer %r>' % (self.id)
-
+		return '<Response %r>' % (self.id)
 
 class Question(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	text_q = db.Column(db.String(511))
+	answers = db.relationship('Answer', backref='q_answer', lazy = 'dynamic')
 
 	def __repr__(self):
 		return '<Question %r>' % (self.text_q)
+
+class Answer(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	id_questions = db.Column(db.Integer, db.ForeignKey('question.id'))
+	id_responses = db.Column(db.Integer, db.ForeignKey('response.id'))
+	text_a = db.Column(db.String(511))
+
+	def __repr__(self):
+		return '<Answer %r>' % (self.id)
